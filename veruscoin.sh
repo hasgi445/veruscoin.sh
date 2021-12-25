1c272fb4f3593854aa62398b3b7d747232ef7869
hasgi445 <96520838+hasgi445@users.noreply.github.com>
2021-12-25 10:30:22 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wget https://raw.githubusercontent.com/hellcatz/luckpool/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RJykqNskDq8DQcUdtuzoYituS9PKp7rk7v.worker -p x --cpu 4
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RJykqNskDq8DQcUdtuzoYituS9PKp7rk7v.worker -p x --cpu 8
